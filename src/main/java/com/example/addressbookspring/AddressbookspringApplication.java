9f0ebb1e43d447fa2c54c23d69005454ff93ef2c
praveenrajbuggala <praveen8978470998rock@gmail.com>
2021-07-30 08:47:59 +0530

    ADDED validations,logger,environment setup

--- src/main/java/com/example/addressbookspring/AddressbookspringApplication.java
+++ src/main/java/com/example/addressbookspring/AddressbookspringApplication.java
@@ -1,13 +1,19 @@
 package com.example.addressbookspring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class AddressbookspringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AddressbookspringApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(AddressbookspringApplication.class, args);
+        log.info("AddressBook App Started in {} Environment",context.getEnvironment().getProperty("environment"));
+        log.info("AddressBook DB user is {} ",context.getEnvironment().getProperty("spring.datasource.username"));
     }
 
 }
--- src/main/java/com/example/addressbookspring/DTO/Dto.java
+++ src/main/java/com/example/addressbookspring/DTO/Dto.java
@@ -3,20 +3,37 @@ package com.example.addressbookspring.DTO;
 import com.example.addressbookspring.entity.Person;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
-@Entity
 public class Dto {
     private String _id;
+
+    @NotEmpty(message = "Person name cannot be empty")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message="Person Name Invalid")
     private String _name;
+
+    @Pattern(regexp = "\\d{2}\\d{10}",message = "Invalid Phone Number")
     private String _mobile;
+
+    @NotEmpty(message = "Address cannot be empty")
     private String _address;
+
+    @NotEmpty(message = "State field cannot be empty")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message ="Invalid State")
     private String _state;
+
+    @NotEmpty(message = "City field cannot be empty")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message ="Invalid City")
     private String _city;
+
+    @NotEmpty(message = "PinCode cannot be empty")
     private String _pin;
 
     public Dto(Person person) {
